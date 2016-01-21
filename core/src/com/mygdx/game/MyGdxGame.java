5d2239fd1b7b2ab62a7b0e2289ef51b433197cb9
Tristan Johnson <m3ltaw4y@gmail.com>
2016-01-21 01:08:23 -0800

    First

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -18,8 +18,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		System.setProperty("javax.net.debug","all");
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		if (isServer) {
-			//SSLServerThread sslst = new SSLServerThread(13769);
-			//sslst.start();
+			SSLServerThread sslst = new SSLServerThread(13769);
+			sslst.start();
 		} else {
 			SSLClientThread sslct = new SSLClientThread("23.119.224.210", 13769);
 			sslct.start();
